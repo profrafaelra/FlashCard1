0e91be19159a56884de051d11cd42465a887b777
profrafaelra <159481560+profrafaelra@users.noreply.github.com>
2024-09-19 17:02:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Argentina?',
+    'É Buenos Aires'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos EUA?',
+    'É Washington'
+)
+
+criaCartao(
+    'Inglês',
+    'O que significa width?',
+    'Largura'
+)
